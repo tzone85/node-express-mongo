d04e02086d78dfef20fd6b159f619b111191af9c
tzone85 <tzone85@gmail.com>
2019-07-08 19:58:31 +0200

    added request parameters and query parameters previous commit

--- src/routes/person.js
+++ src/routes/person.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/person', (req, res) => {
-   res.send('You have requested a person');
+   res.send('You have requested a person. ');
 });
 
 module.exports = router;
