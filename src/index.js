03a827bf3514109a3ea1976fbfe104def6b8b74f
tzone85 <tzone85@gmail.com>
2019-07-08 19:25:28 +0200

    created public folder, created html file, configured express to serve static content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-let express = require('express')
+let express = require('express');
 
 let app = express();
 
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
