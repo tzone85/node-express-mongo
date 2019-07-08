eb13f0a5116dc92493784ac852a1e79e8b3a5f9c
tzone85 <tzone85@gmail.com>
2019-07-08 19:55:12 +0200

    workspace

--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,24 @@
 let express = require('express');
 let router = express.Router();
 
+// QueryString => query property on the request object
+// e.g. localhost:5000/person?name=Mncedi&age=20
 router.get('/person', (req, res) => {
-   res.send('You have requested a person');
+
+   if(req.query.name){
+      res.send(`You have requested a person ${req.query.name}`);
+   }
+   else{
+      res.send('You have requested a person');
+   }
+
+});
+
+// params property on the request object
+// e.g. loclhost:5000/person/Mncedi
+router.get('/person/:name', (req, res) => {
+
+   res.send(`You have requested a person ${req.params.name}`);
 });
 
 module.exports = router;
