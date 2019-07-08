d0ca9d43d6488929fb395d233518d2aabe2cce9d
tzone85 <tzone85@gmail.com>
2019-07-08 21:47:42 +0200

    final touches on how to the product

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -31,5 +31,61 @@ router.post('/customer', (req, res) => {
         })
 });
 
+// add more error loging and routing
+
+//GET
+
+router.get('/customer', (req, res) => {
+
+    if (!req.query.email) {
+        return res.status(400).send('Missing url parameter email');
+    }
+
+    CustomerModel.findOne({
+        email: req.query.email
+    })
+    .then(doc => {
+        res.json(doc)
+    })
+});
+
+// UPDATE
+router.put('/customer', (req, res) => {
+
+    if (!req.query.email) {
+        return res.status(400).send('Missing url parameter email');
+    }
+
+    CustomerModel.findOneAndUpdate({
+        email: req.query.email
+    }, req.body, {
+        new: true
+    })
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
+
+// DELETE
+
+router.delete('/customer', (req, res) => {
+
+    if (!req.query.email) {
+        return res.status(400).send('Missing url parameter email');
+    }
+
+    CustomerModel.findOneAndRemove({
+        email: req.query.email
+    })
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
 
 module.exports = router;
